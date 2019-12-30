dd1f31b45ac6ef3b953d1ef78b265e60366bc404
sprkumar <ravindrakumarjrd@example.com>
2019-12-30 16:34:42 +0530

    All file added

---
+++ src/main/java/com/rk/paytint/SpringBootPayTmIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.rk.paytint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPayTmIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPayTmIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rk/paytint/SpringBootPayTmIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rk.paytint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPayTmIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
